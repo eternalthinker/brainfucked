824b592526c8e121d1684a2cf79235046caf4472
Rahul Anand <et@eternal-thinker.com>
2014-11-14 23:33:48 +0530

    Indentation to spaces -_-

--- manager.js
+++ manager.js
@@ -11,10 +11,10 @@ var outputUi = null;
 var inputUi = null;
 var runtimeUi = null;
 var State = Object.freeze({
-	RUNNING: 1,
-	PAUSED: 2,
-	STOPPED: 3,
-	READ: 4
+    RUNNING: 1,
+    PAUSED: 2,
+    STOPPED: 3,
+    READ: 4
 });
 var curState = State.STOPPED;
 
@@ -23,130 +23,130 @@ if(typeof(w) == "undefined") {
 }
 
 w.onmessage = function(event){
-	var data = event.data;
+    var data = event.data;
 
-	switch (data.command) {
-		case "print": {
-			outputUi.value += data.value;
-			break;
-		}
-		case "read": {
-			setState(State.READ);
-			promptInput();
-			break;
-		}
-		case "error": {
-			setState(State.STOPPED);
-			showError(data.message);
-			break;
-		}
-		case "fin": {
-			runtimeUi.innerHTML = data.runtime;
-			stop();
-			break;
-		}
-		default:
-			break;
-	}
+    switch (data.command) {
+        case "print": {
+            outputUi.value += data.value;
+            break;
+        }
+        case "read": {
+            setState(State.READ);
+            promptInput();
+            break;
+        }
+        case "error": {
+            setState(State.STOPPED);
+            showError(data.message);
+            break;
+        }
+        case "fin": {
+            runtimeUi.innerHTML = data.runtime;
+            stop();
+            break;
+        }
+        default:
+            break;
+    }
 };
 
 
 function run()
 {
-	if (curState === State.STOPPED) {
-		start();
-	}
-	else {
-		resume();
-	}
-	setState(State.RUNNING);
+    if (curState === State.STOPPED) {
+        start();
+    }
+    else {
+        resume();
+    }
+    setState(State.RUNNING);
 }
 
 function start()
 {
-	outputUi = document.getElementById("output");
-	inputUi = document.getElementById("input");
-	runtimeUi = document.getElementById("runtime");
-	errorUi = document.getElementById("error");
-	runButton = document.getElementById("run");
-	resumeButton = document.getElementById("resume");
-	stopButton = document.getElementById("stop");
-	pauseButton = document.getElementById("pause");
-	optimizeCheck = document.getElementById("optimize");
+    outputUi = document.getElementById("output");
+    inputUi = document.getElementById("input");
+    runtimeUi = document.getElementById("runtime");
+    errorUi = document.getElementById("error");
+    runButton = document.getElementById("run");
+    resumeButton = document.getElementById("resume");
+    stopButton = document.getElementById("stop");
+    pauseButton = document.getElementById("pause");
+    optimizeCheck = document.getElementById("optimize");
 
-	outputUi.value = "";
-	runtimeUi.innerHTML = "";
-	errorUi.innerHTML = "";
-	var program = document.getElementById("program").value;
-	var input = inputUi.value;
-	var optimize = optimizeCheck.checked;
-	
-	setState(State.RUNNING);
-	w.postMessage({ "command": "run", "program": program, "input": input, "optimize": optimize });
+    outputUi.value = "";
+    runtimeUi.innerHTML = "";
+    errorUi.innerHTML = "";
+    var program = document.getElementById("program").value;
+    var input = inputUi.value;
+    var optimize = optimizeCheck.checked;
+    
+    setState(State.RUNNING);
+    w.postMessage({ "command": "run", "program": program, "input": input, "optimize": optimize });
 }
 
 function setState(state) {
-	curState = state;
-	switch (curState) {
-		case State.RUNNING: {
-			runButton.disabled = true;
-			stopButton.disabled = false;
-			pauseButton.disabled = false;
-			resumeButton.disabled = true;
-			optimizeCheck.disabled = true;
-			break;
-		}
-		case State.READ:
-		case State.PAUSED: {
-			runButton.disabled = true;
-			stopButton.disabled = false;
-			pauseButton.disabled = true;
-			resumeButton.disabled = false;
-			optimizeCheck.disabled = true;
-			break;
-		}
-		case State.STOPPED: {
-			runButton.disabled = false;
-			stopButton.disabled = true;
-			pauseButton.disabled = true;
-			resumeButton.disabled = true;
-			optimizeCheck.disabled = false;
-			break;
-		}
-		default:
-			break;
-	}
+    curState = state;
+    switch (curState) {
+        case State.RUNNING: {
+            runButton.disabled = true;
+            stopButton.disabled = false;
+            pauseButton.disabled = false;
+            resumeButton.disabled = true;
+            optimizeCheck.disabled = true;
+            break;
+        }
+        case State.READ:
+        case State.PAUSED: {
+            runButton.disabled = true;
+            stopButton.disabled = false;
+            pauseButton.disabled = true;
+            resumeButton.disabled = false;
+            optimizeCheck.disabled = true;
+            break;
+        }
+        case State.STOPPED: {
+            runButton.disabled = false;
+            stopButton.disabled = true;
+            pauseButton.disabled = true;
+            resumeButton.disabled = true;
+            optimizeCheck.disabled = false;
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 function resume() {
-	if (curState === State.READ) {
-		var input = inputUi.value;
-		w.postMessage({ "command": "input", "input": input });
-	}
-	else {
-		w.postMessage({ "command": "resume" });	
-	}
-	setState(State.RUNNING);
+    if (curState === State.READ) {
+        var input = inputUi.value;
+        w.postMessage({ "command": "input", "input": input });
+    }
+    else {
+        w.postMessage({ "command": "resume" }); 
+    }
+    setState(State.RUNNING);
 }
 
 function stop() {
-	setState(State.STOPPED);
-	w.postMessage({ "command": "halt" });
+    setState(State.STOPPED);
+    w.postMessage({ "command": "halt" });
 }
 
 function pause() {
-	setState(State.PAUSED);
-	w.postMessage({ "command": "halt" });	
+    setState(State.PAUSED);
+    w.postMessage({ "command": "halt" });   
 }
 
 function showError(message) {
-	errorUi.innerHTML = message;
+    errorUi.innerHTML = message;
 }
 
 function promptInput() {
-	inputUi.value = "";
-	// log consumed input
-	// check for valid input (ascii)
-	alert("Enter input..");
+    inputUi.value = "";
+    // log consumed input
+    // check for valid input (ascii)
+    alert("Enter input..");
 }
 
